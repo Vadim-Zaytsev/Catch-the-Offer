a125755374deb3f9d3d4e88c2dc0080ce605613a
Vadim Zaytsev <z.v.m.365@gmail.com>
2024-03-06 17:57:04 +0300

    Added sound when clicking on an offer

--- components/Game/Offer/offer.component.js
+++ components/Game/Offer/offer.component.js
@@ -1,11 +1,15 @@
 import { clickHandler, data } from '../../../data/data.js';
 
 export function Offer() {
+    const audio = new Audio('../../../assets/sound/catch.mp3');
+
     const offerElement = document.createElement('img');
 
     offerElement.src = data.offer.offerUrl;
 
     offerElement.addEventListener('click', () => {
+        audio.play();
+
         clickHandler();
     });
 
